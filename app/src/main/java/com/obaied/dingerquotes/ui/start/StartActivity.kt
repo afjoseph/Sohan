3cb17e53192dbdb381e7bd9d45bad836ce6ea1b5
afjoseph <abdullahfjoseph@gmail.com>
2017-09-21 14:42:47 +0200

    Add fabric

--- app/src/main/java/com/obaied/dingerquotes/ui/start/StartActivity.kt
+++ app/src/main/java/com/obaied/dingerquotes/ui/start/StartActivity.kt
@@ -1,34 +1,45 @@
 package com.obaied.dingerquotes.ui.start
 
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.Toast
+import com.crashlytics.android.Crashlytics
+import com.obaied.colours.Colour
 import com.obaied.dingerquotes.R
 import com.obaied.dingerquotes.data.model.Quote
 import com.obaied.dingerquotes.ui.base.BaseActivity
+import com.obaied.dingerquotes.ui.custom.ColorScrollListener
 import com.obaied.dingerquotes.ui.custom.InfiniteScrollListener
 import com.obaied.dingerquotes.ui.quote.QuoteActivity
 import com.obaied.dingerquotes.util.d
 import com.obaied.dingerquotes.util.i
+import io.fabric.sdk.android.Fabric
 import kotlinx.android.synthetic.main.activity_start.*
 import javax.inject.Inject
-
 
 class StartActivity : BaseActivity(), StartMvpView {
     companion object {
         val QUOTE_LIMIT_PER_PAGE = 30
+        val KEY_SCROLL_STATE = "KEY_SCROLL_STATE"
     }
 
     @Inject lateinit var mPresenter: StartPresenter
     @Inject lateinit var mQuotesAdapter: QuotesAdapter
 
+    var gradient: GradientDrawable? = null
+    var scrollState: Parcelable? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         d { "startActivity: onCreate() " }
 
         super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
         setContentView(R.layout.activity_start)
 
         activityComponent()?.inject(this)
